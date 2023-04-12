e8b9635dd2758de8b8d734f73be777ed38656778
ᄒᄋ <jepiros555@gmail.com>
2023-04-12 02:13:24 +0000

    2023/04/12 수정

--- js/basic_test.js
+++ js/basic_test.js
@@ -22,13 +22,13 @@
 // //const f  // Error?
 // console.log(e);
 
-let number = 5;
-let str = "문자열 입력"; // “ “도 묶음 가능
-let prime = 1.5123;
-let is_ok = true; // 참
-let is_not = false; // 거짓
-let undefi; // 변수 이름만, 초기화 x
-let empty = null; // 비어 있음
+// let number = 5;
+// let str = "문자열 입력"; // “ “도 묶음 가능
+// let prime = 1.5123;
+// let is_ok = true; // 참
+// let is_not = false; // 거짓
+// let undefi; // 변수 이름만, 초기화 x
+// let empty = null; // 비어 있음
 
-console.log(number);
+// console.log(number);
 
