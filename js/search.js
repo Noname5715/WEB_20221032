d2dd3c6445dd603f4b6e2ae79819b7164e0eb918
ᄒᄋ <jepiros555@gmail.com>
2023-04-19 02:26:22 +0000

    2023/04/19 버그씹망

--- js/search.js
+++ js/search.js
@@ -1,8 +1,20 @@
 document.getElementById("search_btn").addEventListener('click', search_message);
 
+var search_array=[];
+
+
 function search_message(){
-   alert("검색을 수행합니다!");
    let search_str = document.querySelector("#search_txt"); // 변수에 저장
-   document.getElementById("search_message").innerHTML = search_str.value; // 태그에 값 추가
+	if(search_str.value.length==0){
+		alert("검색어가 비어있습니다. 입력해주세요.");
+	}
+	else{
+		alert("검색을 수행합니다!");
+		search_array.push(search_str.value);
+		let text = document.getElementById("search_message").innerHTML = search_array.toString(); // 값 변환
+        document.querySelector("#form_main").submit();
+		//document.getElementById("search_message").innerHTML = search_str.value; // 태그에 값 추가
+	}
+  
    console.log(search_str.value); // 콘솔에 출력
 }
