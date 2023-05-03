3397365e2fcf75566e2d4faabe0c3e1cb264daa5
ᄒᄋ <jepiros555@gmail.com>
2023-05-03 02:21:17 +0000

    2023/05/03

--- js/search.js
+++ js/search.js
@@ -1,10 +1,14 @@
 document.getElementById("search_btn").addEventListener('click', search_message);
 
 var search_array=[];
+var i=0;
 
-
+//search_message
 function search_message(){
    let search_str = document.querySelector("#search_txt"); // 변수에 저장
+	if(i>9){
+		search_array.shift();
+	}
 	if(search_str.value.length==0){
 		alert("검색어가 비어있습니다. 입력해주세요.");
 	}
@@ -12,9 +16,12 @@ function search_message(){
 		alert("검색을 수행합니다!");
 		search_array.push(search_str.value);
 		let text = document.getElementById("search_message").innerHTML = search_array.toString(); // 값 변환
-        document.querySelector("#form_main").submit();
+        //document.querySelector("#form_main").submit();
 		//document.getElementById("search_message").innerHTML = search_str.value; // 태그에 값 추가
+		i++;
 	}
   
-   console.log(search_str.value); // 콘솔에 출력
+   console.log(search_array); // 콘솔에 출력
+	console.log(i);
+	
 }
